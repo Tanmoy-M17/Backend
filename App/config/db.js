c1cd8de8dda22881094963f70843946e93cb9538
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2023-02-02 19:58:00 +0530

    db

--- App/config/db.js
+++ App/config/db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/agumentic");
+const connection=mongoose.connect("mongodb+srv://tanmoy:tanmoy@cluster0.cmjb5.mongodb.net/agumentic?retryWrites=true&w=majority")
+
 
 module.exports={
     connection
