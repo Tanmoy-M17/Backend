efa675170fe67c5697b2e49f86b379aa27c96719
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2023-02-03 14:17:29 +0530

    Working

--- App/src/index.js
+++ App/src/index.js
@@ -11,12 +11,12 @@ app.use("/user",userrouter)
 app.use("/data",datarouter)
 app.use("/image",imagerouter)
 
-app.listen(8080,async()=>{
+app.listen(4500,async()=>{
     try{
         await connection
         console.log("Server is Connected with Database");
     }catch(err){
         console.log(`Err:${err}`);
     }
-    console.log(`Server is running on port 8080`);
+    console.log(`Server is running on port 4500`);
 })
